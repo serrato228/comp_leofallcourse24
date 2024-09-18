20cf721e0b25895f0a74686ee87f7d751070651c
Leonardo Serrato <leonardo.serrato366@my.tccd.edu>
2024-09-18 18:04:25 -0500

    final Lab 1

--- labs/Lab1/GradeCalculator/GradeCalculator.cpp
+++ labs/Lab1/GradeCalculator/GradeCalculator.cpp
@@ -1,63 +1,91 @@
-// GradeCalculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
 #include <cmath>
 #include <iomanip>
 
 
+
+
+using namespace std;
+
 int main()
 {
-    //Obtains name input
-    std::string name;
-    std::cout << "Please enter your name: ";
-    std::getline(std::cin, name);
-   
+    cout << "Lab 1 --- Leo Serrato --- COSC 1436 Fall 2024" << endl;
+    cout << endl;
 
+    //Obtains name
+    string name;
+    cout << "Please enter your name: ";
+    getline(cin, name);
 
+    //Obtains Lab Grades
     int lab1Grade;
     int lab2Grade;
     int lab3Grade;
     int lab4Grade;
-    double numberOfLabs = 4;
-    std::cout << "Please enter lab 1: ";
-    std::cin >> lab1Grade;
-    std::cout << "Please enter lab 2: ";
-    std::cin >> lab2Grade;
-    std::cout << "Please enter lab 3: ";
-    std::cin >> lab3Grade;
-    std::cout << "Please enter lab 4: ";
-    std::cin >> lab4Grade;
-    double labsAverage = ((lab1Grade + lab2Grade + lab3Grade + lab4Grade) / numberOfLabs);
-
+    double numberOfLabs = 4; //by dividing ints by a double, I can maintain the desired precision decimals
+    cout << "Please enter lab 1: ";
+    cin >> lab1Grade;
+    cout << "Please enter lab 2: ";
+    cin >> lab2Grade;
+    cout << "Please enter lab 3: ";
+    cin >> lab3Grade;
+    cout << "Please enter lab 4: ";
+    cin >> lab4Grade;
+    double labsAverage = ((lab1Grade + lab2Grade + lab3Grade + lab4Grade) / numberOfLabs); // Finds the mean by adding lab grades and dividing by their amount of grades
+    
+    //Obtains Exam Grades
     int exam1Grade;
     int exam2Grade;
     int exam3Grade;
-    double numberOfExams = 3;
-    std::cout << "Please enter exam 1: ";
-    std::cin >> exam1Grade;
-    std::cout << "Please enter exam 2: ";
-    std::cin >> exam2Grade;
-    std::cout << "Please enter exam 3: ";
-    std::cin >> exam3Grade;
-    double examsAverage = ((exam1Grade + exam2Grade + exam3Grade) / numberOfExams); 
+    double numberOfExams = 3; //same thing as what I did for labs
+    cout << "Please enter exam 1: ";
+    cin >> exam1Grade;
+    cout << "Please enter exam 2: ";
+    cin >> exam2Grade;
+    cout << "Please enter exam 3: ";
+    cin >> exam3Grade;
+    double examsAverage = ((exam1Grade + exam2Grade + exam3Grade) / numberOfExams);
 
-    std::cout << (name) << ", your lab grades are: " << std::endl;
-    std::cout << "Lab 1 = " << (lab1Grade) << std::endl;
-    std::cout << "Lab 2 = " << (lab2Grade) << std::endl;
-    std::cout << "Lab 3 = " << (lab3Grade) << std::endl;
-    std::cout << "Lab 4 = " << (lab4Grade) << std::endl;
+    //Obtains Participation Grade
+    int participationGrade;
+    cout << "Please enter participation: ";
+    cin >> participationGrade;
 
-    std::cout << (name) << ", your exam grades are: " << std::endl;
-    std::cout << "Exam 1 = " << (exam1Grade) << std::endl;
-    std::cout << "Exam 2 = " << (exam2Grade) << std::endl;
-    std::cout << "Exam 3 = " << (exam3Grade) << std::endl;
+    //Obtains Final Exam Grade
+    int finalExam;
+    cout << "Please enter final exam: ";
+    cin >> finalExam;
+    cout << endl; //this creates a space between my sections, it makes it easier to read and more similar to the sample provided
 
-    std::cout << (name) << ", your class grade is: " << std::endl;
-    std::cout << "Lab's Average (65%) = " << (labsAverage) << std::endl;
-    std::cout << "Exam's Average (20%) = " << (examsAverage) << std::endl;
+    // The user's name is displayed alongside his lab grades and his exam grades, after Story 6, the user's participation and final exam grade are also displayed
+    cout << (name) << ", your lab grades are: " << endl;
+    cout << "Lab 1 = " << (lab1Grade) << endl; //although the parenthesis aren't needed, I decided to use as shown in lines 57-72 as it allowed me to analyze and see my code and variables more clearly, I wont repeat this again, just did it because I didnt want to mess up on my first lab
+    cout << "Lab 2 = " << (lab2Grade) << endl;
+    cout << "Lab 3 = " << (lab3Grade) << endl;
+    cout << "Lab 4 = " << (lab4Grade) << endl;
+    cout << endl;
+
+    cout << (name) << ", your exam grades are: " << endl;
+    cout << "Exam 1 = " << (exam1Grade) << endl;
+    cout << "Exam 2 = " << (exam2Grade) << endl;
+    cout << "Exam 3 = " << (exam3Grade) << endl;
+    cout << endl;
     
-    double percentageLabs = 0.75
-        //double classAverage = ((labsAverage * percentageLabs) + (examsAverage * percentageExams));
+    cout << (name) << ", your other grades are: " << endl;
+    cout << "Participation = " << (participationGrade) << endl;
+    cout << "Final Exam = " << (finalExam) << endl;
+    cout << endl;
+
+    //these doubles are used to obtain the Class Average by having them multiplied against their respective averages and grades, these doubles' percentage values are aqcuired in Story 6
+    double percentageLabs = 0.65;
+    double percentageExams = 0.20;
+    double percentageParticipation = 0.05;
+    double percentageFinalExam = 0.10;
+    cout << (name) << ", your class grade is: " << endl;
+    cout << "Lab's Average (65%) = " << fixed << setprecision(2) << (labsAverage) << "%" << endl;
+    cout << "Exam's Average (20%) = " << (examsAverage) << "%" << endl;
+    cout << "Participation (5%) = " << (participationGrade) << "%" << endl;
+    cout << "Final Exam (10%) = " << (finalExam) << "%" << endl;
+    cout << "Class Average = " << ((labsAverage * percentageLabs) + (examsAverage * percentageExams) + (participationGrade * percentageParticipation) + (finalExam * percentageFinalExam)) << "%" << endl; // I was in between creating a Class Average variable and assigning it the value of the () to the left of the comment, however I decided to not since it is the same thing in this case
 }
