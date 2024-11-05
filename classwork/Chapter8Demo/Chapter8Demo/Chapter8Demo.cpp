021c4525dc6df7dbff5350af7bfe46137241c177
Leonardo Serrato <leonardo.serrato366@my.tccd.edu>
2024-11-04 21:16:23 -0600

    pointers

---
+++ classwork/Chapter8Demo/Chapter8Demo/Chapter8Demo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+//pointer - memory adds
+
+void StackDemo()
+{
+    double localDouble = 45.6;
+
+    //Declares a pointer to a double
+    // Pointer is always a memory address
+    // Pointer is always eitehr 4 or 8 bytes long
+    // Pointer values is always an integral value
+    
+    // Pointers have 2 data points: value (memory address), value they reference (in memory)
+    double* ptrDouble;
+
+};
+
+int main()
+{
+    StackDemo();
+}
