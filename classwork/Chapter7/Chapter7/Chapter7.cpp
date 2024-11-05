bc18c814fb178852f4017195f17ebdec9688a168
Leonardo Serrato <leonardo.serrato366@my.tccd.edu>
2024-11-04 19:32:14 -0600

    caught up

--- classwork/Chapter7/Chapter7/Chapter7.cpp
+++ classwork/Chapter7/Chapter7/Chapter7.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <iomanip>
@@ -98,16 +99,110 @@ void GradeArrayDemo()
 void InitArrayDemo()
 {
     const int MaxRates = 100;
-    double payRates[MaxRates] = {0};  //Zero initialize
+    double payRates[MaxRates] = {0};  //Zero initialize using an init expr
 
-    ////Zero init
+    ////Zero init using a for loop
     //for (int index = 0; index < MaxRates; ++index)
     //    payRates[index] = 0;
+
+    //Init days of month
+    //char name[] = "Bob";
+    std::string monthNames[12] = {"Jan", "Feb", "Mar", "Apr", "May",
+                                   "Jun", "Jul", "Aug", "Sep", "Oct",
+                                   "Nov", "Dec"};
+    /*monthNames[0] = "Jan";
+    monthNames[1] = "Feb";
+    monthNames[2] = "Mar";
+    monthNames[3] = "Apr";*/
+
+    // Zero extend initialize all elements
+    //int daysInMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    /*daysInMonth[0] = daysInMonth[2] = daysInMonth[4] = daysInMonth[6] =
+        daysInMonth[7] = daysInMonth[9] = daysInMonth[11] = 31;
+    daysInMonth[1] = 28;
+    daysInMonth[3] = daysInMonth[5] = daysInMonth[8] = daysInMonth[10] = 30;
+    daysInMonth[12] = 10;
+    */
+
+    for (int index = 0; index < 12; ++index)
+        std::cout << monthNames[index] << " has " << daysInMonth[index] << " days" << std::endl;
+}
+
+// Function parameters `T id[]`
+//   Always open array, no size
+//   Size is always needed as param when using arrays
+bool AreArraysEqual(int left[], int leftSize, int right[], int rightSize)
+{
+    if (leftSize != rightSize)
+        return false;
+
+    for (int index = 0; index < leftSize; ++index)
+        if (left[index] != right[index])
+            return false;
+
+    return true;
+};
+
+int CopyArray(int left[], int leftSize, int right[], int rightSize)
+{
+    int size = (leftSize < rightSize) ? leftSize : rightSize;
+    for (int index = 0; index < size; ++index)
+        left[index] = right[index];
+
+    return size;
+};
+
+//Array cannot be return type from function
+void InitializeArray(int values[], int size, int initialValue)
+{
+    for (int index = 0; index < size; ++index)
+        values[index] = initialValue;
+};
+
+void UseArrayDemo()
+{
+    int array1[20] = {0};
+
+    //for (int index = 0; index < 20; ++index)
+    //    array1[index] = index + 1;
+    /*for (int& value: array1)
+        value = 1;*/
+    InitializeArray(array1, 20, 2);
+
+    //int index2 = 0;
+    //while (index2 < 20)
+    //    array1[index2++] = index2 + 1;
+    //    /*array1[index2] = index2 + 1;
+    //    ++index2;*/
+
+    // For loop for iterating arrays
+    //for (int index = 0; index < 20; ++index)
+    //   std::cout << array1[index] << std::endl;
+    // For range for iterating arrays
+    //   Copies elements into temp variable like pass by value parameters
+    //   Only works if array declared in same scope
+    //   Can use T& to allow modifications to array element, like pass by ref parameters
+    for (int value : array1)
+        std::cout << value << std::endl;
+
+    int array2[20] = {0};
+    int array3[20] = {0};
+    //array3[19] = 10;
+
+    //Compare arrays
+    std::cout << (array2 == array3)
+        << " " << AreArraysEqual(array2, 20, array3, 20)
+        << std::endl;
+
+//Assignment
+//array3 = array2;
+    CopyArray(array3, 20, array2, 20);
 }
 
 int main()
 {
-    InitArrayDemo();
+    UseArrayDemo();
 }
 
 void NameArrayDemo()
